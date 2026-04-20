bc96b6f6c5371b73ffe53e8ec726c77c2d9a157d
Masaya Hori <masaya.hori@gmail.com>
2026-04-21 05:42:28 +0900

    Add magicLink plugin

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,20 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
-// If your Prisma file is located elsewhere, you can change the path
-// import { PrismaClient } from "@/generated/prisma/client";
+import { magicLink } from "better-auth/plugins";
 
-// const prisma = new PrismaClient();
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
+  plugins: [
+    // add plugins here
+    magicLink({
+      // options for magic link plugin
+      sendMagicLink: async ({ email, token }) => {
+        // implement your logic to send the magic link email here
+        console.log(`Send magic link to ${email} with token: ${token}`);
+      },
+    }),
+  ],
 });
