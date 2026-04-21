7be3e122977785b2dcce7121795f5f1870473fba
Masaya Hori <masaya.hori@gmail.com>
2026-04-22 05:45:46 +0900

    Create organisations page - /organisations/create - instal shadcn ui

---
+++ app/organisations/create/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>Create Organisation</h1>
+      {/* Form for creating an organisation goes here */}
+    </main>
+  );
+}
---
+++ app/organisations/create/ui/organisation-form.tsx
@@ -0,0 +1,8 @@
+export default function OrganisationForm() {
+  return (
+    <div>
+      <h1>Create Organisation</h1>
+      {/* Form for creating an organisation goes here */}
+    </div>
+  );
+}
