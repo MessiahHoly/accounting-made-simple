14f25aacc689a9f2dc50254e7e9836d1cb86417b
Masaya Hori <masaya.hori@gmail.com>
2026-04-22 06:17:29 +0900

    Create organisation form

--- app/organisations/create/page.tsx
+++ app/organisations/create/page.tsx
@@ -1,8 +1,11 @@
+import { OrganisationForm } from "./ui/organisation-form";
+
 export default function Page() {
   return (
     <main>
       <h1>Create Organisation</h1>
       {/* Form for creating an organisation goes here */}
+      <OrganisationForm />
     </main>
   );
 }
