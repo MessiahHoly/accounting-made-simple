ab7b9bfac79947f0ee4cf82a06bd38c3b7ad61a7
Masaya Hori <masaya.hori@gmail.com>
2026-04-24 07:29:58 +0900

    Center sign in form

--- app/organisations/create/page.tsx
+++ app/organisations/create/page.tsx
@@ -6,18 +6,18 @@ import { getSession } from "@/lib/server/utils";
 
 export default async function Page() {
   const session = await getSession();
-  // const session = await auth.api.getSession({ headers: await headers() });
 
   if (!session) {
     return (
-      <main className="w-full p-4">
+      <main className="w-full max-w-md px-4">
+      {/* <main className="w-full p-4"> */}
         <h1>Sign In</h1>
         <SignInForm />
       </main>
     );
   }
   return (
-    <main>
+    <main className="w-full max-w-md px-4">
       <h1>Create Organisation</h1>
       <OrganisationForm />
     </main>
