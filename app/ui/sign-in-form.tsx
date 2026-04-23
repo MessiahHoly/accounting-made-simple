4495cf76cb2a134e94c50e978470064adf924499
Masaya Hori <masaya.hori@gmail.com>
2026-04-24 07:36:47 +0900

    Add spinner to sign in button

--- app/ui/sign-in-form.tsx
+++ app/ui/sign-in-form.tsx
@@ -8,12 +8,14 @@ import {
   FieldSet,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { Spinner } from "@/components/ui/spinner";
 import { authClient } from "@/lib/auth-client";
 import { redirect, usePathname, useSearchParams } from "next/navigation";
 import { useState } from "react";
 
 export function SignInForm() {
   const [email, setEmail] = useState("");
+  const [signingIn, setSigningIn] = useState(false);
   const searchParams = useSearchParams();
   const params = new URLSearchParams(searchParams);
   const pathname = usePathname();
@@ -21,14 +23,17 @@ export function SignInForm() {
 
   const handleSignIn = async (e: React.SubmitEvent) => {
     e.preventDefault();
+    setSigningIn(true);
     // Handle sign-in logic here
     const { data, error } = await authClient.signIn.magicLink({
       email, callbackURL
     })
-    if (error) {      console.error("Error sending magic link:", error);
+    if (error) {
+      console.error("Error sending magic link:", error);
     } else {
       console.log("Magic link sent:", data);
     }
+    // setSigningIn(false);
     redirect('/email-sent');
   };
 
@@ -55,7 +60,19 @@ export function SignInForm() {
             </FieldGroup>
           </FieldSet>
           <Field orientation="horizontal">
-            <Button type="submit">Save</Button>
+            {signingIn ? (
+              <Button disabled>
+                <Spinner data-icon="inline-start" />
+                Signing in...
+              </Button>
+            ) : (
+              <Button>
+                Sign in
+              </Button>
+            )}
+            {/* <Button type="submit" disabled={signingIn}>
+              {signingIn ? "Sending..." : "Save"}
+            </Button> */}
             <Button variant="outline" type="button">
               Cancel
             </Button>
@@ -65,3 +82,4 @@ export function SignInForm() {
     </div>
   )
 }
+//TODO: change width of /email-sent page to match this form
