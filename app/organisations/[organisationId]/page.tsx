6a47cd4e356852b776c4a44eacb2ad123f599feb
Masaya Hori <masaya.hori@gmail.com>
2026-04-26 08:26:08 +0900

    Fetch organisation

--- app/organisations/[organisationId]/page.tsx
+++ app/organisations/[organisationId]/page.tsx
@@ -1,9 +1,15 @@
+import { fetchOrganisation } from "@/lib/data/organisation"
+import { notFound } from "next/navigation"
+
 export default async function Page({ params }: { params: Promise<{ organisationId: string }> }) {
   const { organisationId } = await params
+  const organisation = await fetchOrganisation(organisationId)
+
+  if (!organisation) return notFound()
 
   return (
     <main>
-      <h1>{organisationId}</h1>
+      <h1>{organisation?.name || organisationId}</h1>
     </main>
   )
 }
