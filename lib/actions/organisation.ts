5c04df81653cf539d2b7df5f97f7c248cef9718f
Masaya Hori <masaya.hori@gmail.com>
2026-04-26 08:06:05 +0900

    Be able to create an organisation

--- lib/actions/organisation.ts
+++ lib/actions/organisation.ts
@@ -1,37 +1,37 @@
 'use server'
 
-import { OrganisationCreateInputObjectSchema, OrganisationInputSchema, OrganisationUncheckedCreateInputObjectSchema } from "@/prisma/generated/schemas";
+import {
+  // OrganisationCreateInputObjectSchema, OrganisationInputSchema,
+  OrganisationUncheckedCreateInputObjectSchema
+} from "@/prisma/generated/schemas";
 import { getSession } from "../utils/server/utils";
 import { z } from "zod";
 import { getCleanFormData } from "../utils/utils";
+import prisma from "../prisma";
 
 export const createOrganisation = async (prevState: unknown, formData: FormData) => {
   const session = await getSession();
-  // console.log(session);
   if (!session) {
     return { success: false, message: "Unauthorized" };
   }
-
-  // console.log(formData);
-
-  // const { name } = Object.fromEntries(formData.entries());
-  // const formDataObj = Object.fromEntries(formData.entries());
   const filteredFormData = getCleanFormData(formData);
-  // console.log(filteredFormData);
-  // const validatedData = OrganisationUncheckedCreateInputObjectSchema.safeParse({ ...filteredFormData,  });
   const validatedData = OrganisationUncheckedCreateInputObjectSchema.safeParse({ ...filteredFormData, userId: session.user.id });
-  // const validatedData = OrganisationUncheckedCreateInputObjectSchema.safeParse({ ...formDataObj, userId: session.user.id });
-  // const validatedData = OrganisationUncheckedCreateInputObjectSchema.safeParse({ ...formDataObj, userId: session.user.id });
-  // const validatedData = OrganisationCreateInputObjectSchema.safeParse({ ...formData, userId: session.user.id });
-  // const validatedData = OrganisationCreateInputObjectSchema.safeParse({ name });
-  // console.log(validatedData);
-  // console.log(validatedData);
 
   if (!validatedData.success) {
-    // const { fieldErrors } = z.flattenError(validatedData.error);
     const tree = z.treeifyError(validatedData.error);
-    // console.log("Validation failed:", fieldErrors, tree);
-    // return { success: false, message: "Invalid data", errors: tree };
     return { success: false, message: "Invalid data", errors: tree.properties };
   }
+
+  // Here you would typically call your database to create the organisation
+  // For example:
+  // const newOrganisation = await prisma.organisation.create({
+  //   data: validatedData.data,
+  // });
+
+  const {data} = validatedData
+  await prisma.organisation.create({
+    data
+  });
+
+  return { success: true, message: "Organisation created successfully" };
 }
