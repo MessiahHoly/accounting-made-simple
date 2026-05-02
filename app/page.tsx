937d4b1696411eb746098899318cc698836af165
Masaya Hori <masaya.hori@gmail.com>
2026-05-03 07:22:36 +0900

    Get accounting equations from J-Quants data

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 // import Image from "next/image";
 
 import { fetchJQuantsData } from "@/lib/data/j-quants";
+import { getAccountingEquation } from "@/lib/utils/utils";
 
 // import { fetchJapaneseAccountingData } from "@/lib/data/alpha-vantage";
 
 export default async function Home() {
   // await fetchJapaneseAccountingData("6758.T&apikey=your_api_key"); // Example: Fetch data for Toyota Motor Corporation (7203.T)
   // await fetchJapaneseAccountingData("7203.T"); // Example: Fetch data for Toyota Motor Corporation (7203.T)
-  await fetchJQuantsData("7203"); // Example: Fetch data for Toyota Motor Corporation (7203)
+  const data = await fetchJQuantsData("7203"); // Example: Fetch data for Toyota Motor Corporation (7203)
+  const accountingData = getAccountingEquation(data.data);
+  console.log(accountingData);
 
   return (
     <main>
--- lib/utils/utils.ts
+++ lib/utils/utils.ts
@@ -1,4 +1,5 @@
 import { Decimal } from "@prisma/client/runtime/client";
+import { Code } from "lucide-react";
 
 export const getCleanFormData = (formData: FormData) => {
   const raw = Object.fromEntries(formData.entries());
@@ -7,4 +8,16 @@ export const getCleanFormData = (formData: FormData) => {
 
 export const formatCurrency = (value: Decimal) => {
   return new Intl.NumberFormat("ja-JP", { style: "currency", currency: "JPY" }).format(Number(value))
+}
+
+export const getAccountingEquation = (jQuantsData: {
+  TA: string, Eq: string, CurPerEn: string, Code: string
+}[]) => {
+  console.log("Raw J-Quants data:", jQuantsData);
+  return jQuantsData.map(({ TA, Eq, CurPerEn, Code }) => {
+    const assets = Number(TA);
+    const equity = Number(Eq);
+    const liabilities = assets - equity;
+    return { assets, equity, liabilities, code: Code, asOf: CurPerEn };
+  });
 }
