81f169e9ee016442f6e67a59f544754157407135
Masaya Hori <masaya.hori@gmail.com>
2026-05-05 06:44:45 +0900

    Complete Input component

--- app/organisations/ui/search.tsx
+++ app/organisations/ui/search.tsx
@@ -1,10 +1,22 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   const handleSearch = (term: string) => {
-    console.log("Search query:", term);
+    // console.log("Search query:", term);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("search", term);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathname}?${params.toString()}`);
   };
 
   return (
@@ -12,6 +24,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       placeholder={placeholder}
       onChange={(e) => handleSearch(e.target.value)}
       type="search"
+      defaultValue={searchParams.get("search")?.toString()}
     />
   );
 }
