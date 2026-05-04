784f3ae9aae69f60737886a82218ac93d56995de
Masaya Hori <masaya.hori@gmail.com>
2026-05-04 19:24:18 +0900

    Transform the date

--- lib/types/j-quants.ts
+++ lib/types/j-quants.ts
@@ -3,26 +3,12 @@ import { z } from 'zod'
 export const FinancialSummarySchema = z.object({
   TA: z.string().transform((value) => parseFloat(value)),
   Eq: z.string().transform((value) => parseFloat(value)),
-  CurPerEn: z.string(),
+  CurPerEn: z.string().transform((value) => new Date(value)),
   Code: z.string(),
 })
-// export const FinancialSummarySchema = z.object({
-//   TA: z.string(),
-//   Eq: z.string(),
-//   CurPerEn: z.string(),
-//   Code: z.string(),
-// })
-
-// export const ApiResponseSchema = z.object({
-//   data: z.array(FinancialSummarySchema),
-//   message: z.string(),
-//   status: z.number(),
-// })
 
 export const ApiResponseSchema = z.object({
   data: z.array(FinancialSummarySchema),
 })
 
 export type FinancialSummary = z.infer<typeof FinancialSummarySchema>;
-
-// export type JQuantsApiResponse = z.infer<typeof ApiResponseSchema>;
