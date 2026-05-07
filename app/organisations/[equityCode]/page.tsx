abb69aa5eff7146a53429b1be200691dd755da5e
Masaya Hori <masaya.hori@gmail.com>
2026-05-08 05:36:20 +0900

    Obtain EDINET code

--- app/organisations/[equityCode]/page.tsx
+++ app/organisations/[equityCode]/page.tsx
@@ -5,12 +5,15 @@ import { notFound } from "next/navigation"
 import AccountingEquationRow from "./ui/AccountingEquation"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
-import { fetchBalanceSheet, searchCompany } from "@/lib/data/edinet-db"
+import {
+  // fetchBalanceSheet,
+  searchCompany
+} from "@/lib/data/edinet-db"
 
 export default async function Page({ params }: { params: Promise<{ equityCode: string }> }) {
   const { equityCode } = await params
   // console.log("Fetching data for equity code:", equityCode)
-  const [financeResponse, equityResponse, searchReesponse
+  const [financeResponse, equityResponse, searchResponse
   ] = await Promise.all([
     fetchFinanceSummary(equityCode),
     fetchEquity(equityCode),
@@ -18,7 +21,7 @@ export default async function Page({ params }: { params: Promise<{ equityCode: s
     searchCompany(equityCode)
   ])
 
-  console.log(searchReesponse)
+  // console.log(searchResponse)
 
   if ("error" in financeResponse) {
     console.error("Error fetching finance summary:", financeResponse.error)
@@ -30,10 +33,20 @@ export default async function Page({ params }: { params: Promise<{ equityCode: s
     return <div>Error: {equityResponse.error}</div>
   }
 
+  if ("error" in searchResponse) {
+    console.error("Error searching company:", searchResponse.error)
+    return <div>Error: {searchResponse.error}</div>
+  }
+
   const { data: financeSummaries } = financeResponse
   const { data: equity } = equityResponse
+  const { data: searchResults } = searchResponse
 
-  if (!financeSummaries || financeSummaries.length === 0) return notFound()
+  if (!financeSummaries || financeSummaries.length === 0 || searchResults.length === 0) return notFound()
+  // if (!financeSummaries || financeSummaries.length === 0) return notFound()
+  
+  const { edinet_code } = searchResults[0]
+  // console.log("EDINET code for the company:", edinet_code)
 
   return (
     <main className="p-4">
@@ -49,7 +62,6 @@ export default async function Page({ params }: { params: Promise<{ equityCode: s
         {financeSummaries.map(({ Code, CurPerEn, TA, Eq }) => (
           <AccountingEquationRow
             key={`${Code}-${CurPerEn}`}
-            // key={Code}
             accountingEquation={{ asOf: CurPerEn, assets: Number(TA), liabilities: Number(TA) - Number(Eq), ownersEquity: Number(Eq) }}
           />
         ))}
