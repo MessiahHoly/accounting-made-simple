dea67e09b900239aba5fff814d495f87847c25f4
Masaya Hori <masaya.hori@gmail.com>
2026-05-09 08:21:41 +0900

    Transform dat for balance sheet

--- lib/types/edinet-db.ts
+++ lib/types/edinet-db.ts
@@ -1,5 +1,5 @@
 import { z } from "zod"
-import { meta } from "zod/v4/core"
+// import { meta } from "zod/v4/core"
 
 export const SearchResultSchema = z.object({
   credit_rating: z.string(),
@@ -19,27 +19,6 @@ export const SearchResponseSchema = z.object({
     total: z.int(),
   }),
 })
-
-// export const FinancialsResponseSchema = z.object({
-//   fiscal_year: z.number(),
-//   // Assets
-//   current_assets: z.number(),
-//   noncurrent_assets: z.number(),
-//   total_assets: z.number(),
-//   cash: z.number().optional(),
-//   inventories: z.number().optional(),
-//   // Liabilities
-//   current_liabilities: z.number(),
-//   noncurrent_liabilities: z.number(),
-//   total_liabilities: z.number(),
-//   // Net Assets
-//   net_assets: z.number(),
-//   shareholders_equity: z.number(),
-// }).transform((data) => ({
-//   ...data,
-//   debt_to_equity_ratio: data.total_liabilities / data.shareholders_equity,
-//   isBalanced: Math.abs(data.total_assets - (data.total_liabilities + data.net_assets)) < 1000,
-// }))
 
 export const FinancialsResponseSchema = z.object({
   fiscal_year: z.number(),
@@ -86,23 +65,12 @@ export const FinancialsResponseSchema = z.object({
 }).transform((data) => ({
   ...data,
   // You can keep or update your custom transforms here
+  other_assets: data.total_assets - data.cash,
+  shareholders_equity: data.net_assets,
   debt_to_equity_ratio: data.total_liabilities / data.net_assets,
   isBalanced: Math.abs(data.total_assets - (data.total_liabilities + data.net_assets)) < 1000,
 }));
 
-// export const FinancialsApiResponseSchema = z.object({
-//   data: z.array(FinancialsResponseSchema),
-// })
-// export const FinancialsApiResponseSchema = z.object({
-//   data: z.array(FinancialsResponseSchema),
-//   meta: z.object({
-//     data_notes: z.array(z.string()),
-//     data_source: z.string(),
-//     edinet_code: z.string(),
-//     fiscal_year: z.array(z.number()),
-//     period:z.string(),
-//   }),
-// })
 export const FinancialsApiResponseSchema = z.object({
   data: z.array(FinancialsResponseSchema),
   meta: z.object({
