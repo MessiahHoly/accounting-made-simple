8318042575d48d0ecac58d5c5d739ac11ba8ccac
Masaya Hori <masaya.hori@gmail.com>
2026-05-13 07:06:44 +0900

    Comment out the organistation page

--- app/organisations/page.tsx
+++ app/organisations/page.tsx
@@ -1,21 +1,21 @@
-import Search from "../ui/search";
-import { Suspense } from "react";
-import SearchSkeleton from "../ui/searchSkelton";
-import Equities from "../ui/equities";
+// import Search from "../ui/search";
+// import { Suspense } from "react";
+// import SearchSkeleton from "../ui/searchSkelton";
+// import Equities from "../ui/equities";
 
-export default async function Page(props: { searchParams?: Promise<{ query?: string }> }) {
-  const searchParams = await props?.searchParams;
-  const query = searchParams?.query || "";
+// export default async function Page(props: { searchParams?: Promise<{ query?: string }> }) {
+//   const searchParams = await props?.searchParams;
+//   const query = searchParams?.query || "";
 
-  return (
-    <main className="p-4 w-full max-w-md mx-auto">
-      <h1>Organisations</h1>
-      <div className="flex flex-col gap-10">
-        <Search placeholder="Search organisations..." />
-        <Suspense key={query} fallback={<SearchSkeleton />}>
-          <Equities query={query} />
-        </Suspense>
-      </div>
-    </main>
-  );
-}
+//   return (
+//     <main className="p-4 w-full max-w-md mx-auto">
+//       <h1>Organisations</h1>
+//       <div className="flex flex-col gap-10">
+//         <Search placeholder="Search organisations..." />
+//         <Suspense key={query} fallback={<SearchSkeleton />}>
+//           <Equities query={query} />
+//         </Suspense>
+//       </div>
+//     </main>
+//   );
+// }
