ba5a13363649c39ee2eb58b3243118e2ae5cbe7e
Masaya Hori <masaya.hori@gmail.com>
2026-05-14 07:12:52 +0900

    Avoid getting organisation id

--- app/organisations/organisationId/accounting-equations/create/page.tsx
+++ app/organisations/organisationId/accounting-equations/create/page.tsx
@@ -1,23 +1,43 @@
 import { fetchOrganisation } from "@/lib/data/organisation";
 import { AccountingEquationForm } from "./ui/accounting-equation-form";
 
-export default async function Page({ params }: { params: Promise<{ organisationId: string }> }) {
-  const { organisationId } = await params;
-  const organisation = await fetchOrganisation(organisationId);
+export default async function Page() {
+  // const { organisationId } = await params;
+  // const organisation = await fetchOrganisation(organisationId);
 
-  if (!organisation) {
-    return (
-      <main className="w-full max-w-md px-4">
-        <h1>Organisation Not Found</h1>
-        <p>The organisation you are looking for does not exist.</p>
-      </main>
-    );
-  }
+  // if (!organisation) {
+  //   return (
+  //     <main className="w-full max-w-md px-4">
+  //       <h1>Organisation Not Found</h1>
+  //       <p>The organisation you are looking for does not exist.</p>
+  //     </main>
+  //   );
+  // }
 
   return (
     <main className="w-full max-w-md px-4">
-      <h1>Create Accounting Equation for {organisation.name}</h1>
-      <AccountingEquationForm organisationId={organisationId} />
+      <h1>Create Accounting Equation for </h1>
+      {/* <AccountingEquationForm organisationId={organisationId} /> */}
     </main>
   )
 }
+// export default async function Page({ params }: { params: Promise<{ organisationId: string }> }) {
+//   const { organisationId } = await params;
+//   const organisation = await fetchOrganisation(organisationId);
+
+//   if (!organisation) {
+//     return (
+//       <main className="w-full max-w-md px-4">
+//         <h1>Organisation Not Found</h1>
+//         <p>The organisation you are looking for does not exist.</p>
+//       </main>
+//     );
+//   }
+
+//   return (
+//     <main className="w-full max-w-md px-4">
+//       <h1>Create Accounting Equation for {organisation.name}</h1>
+//       <AccountingEquationForm organisationId={organisationId} />
+//     </main>
+//   )
+// }
