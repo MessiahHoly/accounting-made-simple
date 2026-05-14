e117d9d0d9ad142752ef7625d03e67090c168e7a
Masaya Hori <masaya.hori@gmail.com>
2026-05-15 05:45:57 +0900

    Fix the error around organisationId

--- app/organisations/organisationId/page.tsx
+++ app/organisations/organisationId/page.tsx
@@ -1,25 +1,25 @@
 import { fetchOrganisation } from "@/lib/data/organisation"
-// import { formatCurrency } from "@/lib/utils/utils"
 import { notFound } from "next/navigation"
 import AccountingEquationRow from "./ui/AccountingEquation"
 
-export default async function Page({ params }: { params: Promise<{ organisationId: string }> }) {
-  const { organisationId } = await params
-  const organisation = await fetchOrganisation(organisationId)
+export default async function Page() {
+// export default async function Page({ params }: { params: Promise<{ organisationId: string }> }) {
+  // const { organisationId } = await params
+  // const organisation = await fetchOrganisation(organisationId)
 
-  if (!organisation) return notFound()
+  // if (!organisation) return notFound()
 
-  const { accountingEquations } = organisation
+  // const { accountingEquations } = organisation
 
   return (
     <main className="p-4">
-      <h1>{organisation?.name || organisationId}</h1>
+      {/* <h1>{organisation?.name || organisationId}</h1> */}
       <h2>Accounting Equations</h2>
-      <ul>
+      {/* <ul>
         {accountingEquations.map((eq) => (
           <AccountingEquationRow key={eq.id} accountingEquation={eq} />
         ))}
-      </ul>
+      </ul> */}
     </main>
   )
 }
