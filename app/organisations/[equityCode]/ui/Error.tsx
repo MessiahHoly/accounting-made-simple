769d25f563308bda236bc1c72135b2e5479be329
Masaya Hori <masaya.hori@gmail.com>
2026-06-11 05:45:11 +0900

    Fix error component layout

--- app/organisations/[equityCode]/ui/Error.tsx
+++ app/organisations/[equityCode]/ui/Error.tsx
@@ -4,9 +4,12 @@ export default function Error({ error }: { error?: string }) {
   // console.error("Error in page component:", error)
   return (
     <section>
-      <h1>An error occurred</h1>
-      <p>{error}</p>
-      <BackButton text="Okay..." />
+      {/* <section className="p-4 md:p-8 max-w-screen-2xl mx-auto w-full overflow-hidden"> */}
+      <div className="p-4 md:p-8 max-w-screen-2xl mx-auto w-full overflow-hidden">
+        <h1>An error occurred</h1>
+        <p>{error}</p>
+        <BackButton text="Okay..." />
+      </div>
     </section>
   )
 }
